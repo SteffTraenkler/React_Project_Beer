2a81261585793149d5dbc63d167382ecd13ad6c9
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-2.local>
2022-03-11 13:24:00 +0100

    netlify3

--- src/Components/details.jsx
+++ src/Components/details.jsx
@@ -14,7 +14,7 @@ export default function Details() {
                 console.log(json);
                 setSingleBeer(json)
             })
-    }, [])
+    }, [apiID])
 
     return (
         <div className="detailPage">
